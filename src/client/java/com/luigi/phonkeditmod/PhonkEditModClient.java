bbca4e2fdbef63ac69a1a0f31d78adadc6cb9a80
LuigiLoeck <luigisloeck@gmail.com>
2025-10-18 00:41:45 -0300

    initial commit

---
+++ src/client/java/com/luigi/phonkeditmod/PhonkEditModClient.java
@@ -0,0 +1,10 @@
+package com.luigi.phonkeditmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PhonkEditModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
